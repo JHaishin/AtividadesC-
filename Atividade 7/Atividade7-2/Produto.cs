14546963c22d5bf3458a1b10be907950d078bfc3
JHaishin <150485130+JHaishin@users.noreply.github.com>
2024-01-24 20:39:09 -0300

    Atividade 7-2

---
+++ Atividade 7/Atividade7-2/Produto.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Globalization;
+class Produto {
+public string Nome;
+public double Preco;
+public int Quantidade;
+public double ValorTotalEmEstoque() {
+return Preco * Quantidade;
+}
+public void AdicionarProdutos(int quantidade) {
+Quantidade += quantidade;
+}
+public void RemoverProdutos(int quantidade) {
+Quantidade -= quantidade;
+}
+public override string ToString() {
+return Nome
++ ", $ "
++ Preco.ToString("F2", CultureInfo.InvariantCulture)
++ ", "
++ Quantidade
++ " unidades, Total: $ "
++ ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+}
+}
+
