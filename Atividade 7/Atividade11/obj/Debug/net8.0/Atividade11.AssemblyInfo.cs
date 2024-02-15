f9a6668fd9e4bfcc2e0dc7189824bbf362255e67
JHaishin <150485130+JHaishin@users.noreply.github.com>
2024-02-15 19:46:50 -0300

    Atividade11

--- Atividade 7/Atividade11/obj/Debug/net8.0/Atividade11.AssemblyInfo.cs
+++ Atividade 7/Atividade11/obj/Debug/net8.0/Atividade11.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade10")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Atividade11")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Atividade10")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Atividade10")]
+[assembly: System.Reflection.AssemblyProductAttribute("Atividade11")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Atividade11")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Gerado pela classe WriteCodeFragment do MSBuild.
