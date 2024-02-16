02a966ea9df79915d1fc2693d82ff330c9f75196
JHaishin <150485130+JHaishin@users.noreply.github.com>
2024-02-16 16:39:11 -0300

    Atividade11-2

---
+++ Atividade 7/Atividade11-2/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program {
+    static void Main(string[]args) {
+        Console.WriteLine("Digite um número para a matriz: ");
+        int n = int.Parse(Console.ReadLine());
+        int [,] mat = new int [n,n];
+
+        for (int i=0; i < n; i++) {
+            string[] values = Console.ReadLine().Split(' ');
+
+            for (int o = 0; o< n; o++) {
+                mat[i,o]= int.Parse(values[o]);
+            }
+        }
+
+        Console.WriteLine("Diagonal: ");
+        for (int i=0; i<n; i++) {
+            Console.WriteLine(mat[i,i] + " ");
+        
+        }
+
+        Console.WriteLine();
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int o = 0; o< n; o++) {
+                if (mat[i,o] < 0) {
+                    count++;
+                }
+            }
+        }
+
+        Console.WriteLine("Os números negativos:" + count);
+
+    }
+}
